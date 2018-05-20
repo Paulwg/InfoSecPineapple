bbe8cd9e973119457afa5ef9bf8d920490726fcc
PaulWSterling <paulwg1219@gmail.com>
2018-05-19 17:19:49 -1000

    Bug in s2.Delete()

---
+++ MountVSC.ps1
@@ -0,0 +1,36 @@
+﻿function Mount-ShadowCopy{
+
+    [CmdletBinding()]
+    Param(
+        [Parameter()]
+        [Alias("Remove Shadow Mount")]
+        [switch]$Remove
+    )
+    Begin {}
+    Process{
+        if ($Remove){    
+        
+            try{
+                #$s2.Delete() #<--To unmount the shadow copy/BROKEN
+                [System.IO.Directory]::Delete('C:\shadowcopy') #<---To delete symlink    
+            }
+            catch{
+                Write-Warning -Message "Failed because $($_.Exception.Message)"
+            }
+        }               
+        else{
+            try{
+                $s1 = (Get-WmiObject -List Win32_ShadowCopy).Create("C:\", "ClientAccessible")
+                $s2 = Get-WmiObject Win32_ShadowCopy | Where-Object { $_.ID -eq $s1.ShadowID }
+
+                $d  = $s2.DeviceObject + "\"   # <-- this here
+
+                cmd /c mklink /d C:\shadowcopy "$d"
+           
+            }catch{
+                Write-Warning -Message "Failed to grab mount shadow copy because $($_.Exception.Message)"
+            }                
+        }
+    }
+    End {}
+}
