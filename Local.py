1363e1775c9f6269819e48fd784641827b067eb6
Paul Groce <35648976+Paulwg@users.noreply.github.com>
2018-05-22 10:49:53 -1000

    Initial commit

---
+++ Local.py
@@ -0,0 +1,34 @@
+﻿import subprocess
+
+class Local(object):
+    
+    def AppCompatCache():
+        pass
+    def Process_Table():
+        subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe","get-process"])
+    def Prefetch():
+        pass
+    def Network_Connections():
+        subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe","get-nettcpconnection"])
+    def Auto_Runs(self):
+        pass
+    def Sched_Tasks(self):
+        pass
+    def Startup_Folders(self):
+        pass
+    def Services(self):
+        subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe","get-services"])
+    def Installed_Software(self):
+        pass
+    def Unsigned_Executables(self):
+        pass
+    def Installed_Drivers(self):
+        pass
+    def ADS(self):
+        pass
+    def Dir_Walk(self):
+        pass
+    def VSC_Walk_Timestamps(self):
+        pass
+    def MFT_timestamps(self):
+        pass
