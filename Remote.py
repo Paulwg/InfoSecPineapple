094573b5f203b49d4af8dc9d0fb88bdc94eff154
Paul Groce <35648976+Paulwg@users.noreply.github.com>
2018-05-22 10:50:21 -1000

    Initial commit

---
+++ Remote.py
@@ -0,0 +1,46 @@
+﻿class Remote():
+     
+    def AppCompatCache(self):
+        pass
+    @classmethod
+    def Process_Table(self):
+        subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe","get-process"])
+     
+    def Prefetch(self):
+        pass
+     
+    def Network_Connections(self):
+        subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe","get-nettcpconnection"])
+     
+    def Auto_Runs(self):
+        pass
+     
+    def Sched_Tasks(self):
+        pass
+     
+    def Startup_Folders(self):
+        pass
+     
+    def Services(self):
+        pass
+     
+    def Installed_Software(self):
+        pass
+     
+    def Unsigned_Executables(self):
+        pass
+     
+    def Installed_Drivers(self):
+        pass
+     
+    def ADS(self):
+        pass
+    
+    def Dir_Walk(self):
+        pass
+     
+    def VSC_Walk_Timestamps(self):
+        pass
+     
+    def MFT_timestamps(self):
+        pass
